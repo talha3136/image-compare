b819979ab8ffe03267c4e4fa13d9dfed95615c39
tshahid603 <tshahid603@gmail.com>
2025-02-14 19:30:23 +0500

    update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,16 @@
+# echo "BUILD START"
+# pip install -r requirements.txt
+# python3.12 manage.py collectstatic --noinput --clear
+# echo "BUILD END"
+
+#!/bin/bash
+
 echo "BUILD START"
+
+# Install required packages
 pip install -r requirements.txt
+
+# Collect static files to the correct directory
 python3.12 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
--- image_compare/settings.py
+++ image_compare/settings.py
@@ -125,12 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, 'your_static_directory'),  # Add any custom static folders
 ]
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {'api_key': {'type': 'apiKey', 'in': 'header', 'name': 'Authorization'}},
     'REFETCH_SCHEMA_WITH_AUTH': True,
