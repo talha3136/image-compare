3724d2c168442be7353bd1492d779a446b0d1a4b
tshahid603 <tshahid603@gmail.com>
2025-02-17 15:03:09 +0500

    request error fixed

--- home/services.py
+++ home/services.py
@@ -4,23 +4,42 @@ from urllib.request import urlretrieve
 from django.core.files import File
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
+import requests
+from tempfile import NamedTemporaryFile
+from django.core.files import File
+
 class FileRelatedService:
     
     @staticmethod
     def convert_url_to_file(url):
         if url:
             try:
-                # Verify the URL is reachable before attempting to download
-                with urlopen(url) as response:
-                    if response.status != 200:
-                        return None
-                
+
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                }
+
+
+                response = requests.get(url, headers=headers, stream=True)
+
+                if response.status_code != 200:
+                    print(f"Error: Unable to retrieve file. HTTP Status code: {response.status_code}")
+                    return None
+
+
                 temp_file = NamedTemporaryFile(delete=True)
-                urlretrieve(url, temp_file.name)
+                for chunk in response.iter_content(chunk_size=128):
+                    temp_file.write(chunk)
                 temp_file.seek(0)
+                
                 return File(temp_file)
-            except (URLError, HTTPError) as e:
-                print(f"Error retrieving file from URL: {url}. Error: {e}")
+            
+            except requests.exceptions.RequestException as e:
+                print(f"Request Exception: Error retrieving file from URL: {url}. Error: {e}")
+                return None
+            except Exception as e:
+                print(f"Unexpected error occurred while retrieving file from {url}: {e}")
                 return None
         else:
+            print("Error: URL is None or empty")
             return None
