55f0994460cdc490ab8f2260a8632cbdd1ed1c23
tshahid603 <tshahid603@gmail.com>
2025-02-21 14:18:30 +0500

    improvments

--- home/views.py
+++ home/views.py
@@ -1,24 +1,19 @@
-import shutil
 import os
 
+from home.serializers import ImageSerializer, ImageUrlSerializer
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import io
 import cv2
 import dlib
-from home.serializers import ImageSerializer, ImageUrlSerializer
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageEnhance
 from datetime import datetime
 from deepface import DeepFace
-from django.http import JsonResponse
 from rest_framework import viewsets
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
-
 from home.services import FileRelatedService
 
 # Load dlib face detector and shape predictor
@@ -37,16 +32,12 @@ def is_blurry(image):
     return variance < 100
 
 def enhance_image(image):
-    lab = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
-    l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
-    l = clahe.apply(l)
-    lab = cv2.merge((l, a, b))
-    contrast_enhanced = cv2.cvtColor(lab, cv2.COLOR_Lab2BGR)
-    sharpen_kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    sharpened = cv2.filter2D(contrast_enhanced, -1, sharpen_kernel)
-    denoised = cv2.bilateralFilter(sharpened, d=9, sigmaColor=50, sigmaSpace=50)
-    return denoised
+    pil_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    enhancer = ImageEnhance.Sharpness(pil_image)
+    sharp_image = enhancer.enhance(2.0)
+    enhancer = ImageEnhance.Contrast(sharp_image)
+    contrast_image = enhancer.enhance(1.5)
+    return cv2.cvtColor(np.array(contrast_image), cv2.COLOR_RGB2BGR)
 
 def preprocess_and_save_image(image_bytes, filename):
     try:
@@ -63,52 +54,72 @@ def preprocess_and_save_image(image_bytes, filename):
         return None
 
 def compare_faces(image1_path, image2_path):
+    # models = ["VGG-Face", "Facenet", "Facenet512", "DeepFace", "OpenFace", "ArcFace"]
+    models = [ "Facenet512",  "OpenFace", "ArcFace"]
+
+    results = []
     try:
-        result = DeepFace.verify(img1_path=image1_path, img2_path=image2_path, model_name="ArcFace", detector_backend="retinaface")
-        return result['verified'], f"{(1 - result['distance']) * 100:.2f}"
-    except Exception as e:
-        return False, "0.0"
+        for model_name in models:
+            try:
+                result = DeepFace.verify(img1_path=image1_path, img2_path=image2_path, model_name=model_name, detector_backend="retinaface")
+                verified = result.get('verified', False)
+                distance = result.get('distance', None)
+                results.append((model_name, verified, distance))
+            except Exception as e:
+                results.append((model_name, False, None))
+        verified_count = sum(1 for _, verified, _ in results if verified)
+        total_models = len(models)
+        result = verified_count >= total_models / 2
+        confidence = sum([(1 - distance) * 100 if distance is not None else 0 for _, _, distance in results]) / len(results)
+        note = "Comparison successful and faces match closely." if result else "Comparison successful but faces do not match closely."
+        return result, f"{confidence:.2f}", note
+    finally:
+        if os.path.exists(image1_path):
+            os.remove(image1_path)
+        if os.path.exists(image2_path):
+            os.remove(image2_path)
 
 class CompareImagesViewSet(viewsets.GenericViewSet):
     serializer_class = ImageSerializer
     parser_classes = (MultiPartParser, FormParser)
-
-    @action(detail=False, methods=['POST'], url_path='compare', serializer_class=ImageUrlSerializer)
+    
+    @action(
+        detail=False, 
+        methods=['POST'],
+        url_path='compare', 
+        serializer_class=ImageUrlSerializer
+    )
     def compare(self, request):
         profileImageURL = request.data.get('profileImageURL')
         targetImageURL = request.data.get('targetImageURL')
-
         profileImagePath = FileRelatedService.convert_url_to_file(profileImageURL)
         targetImagePath = FileRelatedService.convert_url_to_file(targetImageURL)
-
         if profileImagePath and targetImagePath:
             try:
-                # Open the file at the file path and read its content
                 with open(profileImagePath, 'rb') as profileImage:
-                    image1_bytes = profileImage.read()  # Read file content
-
+                    image1_bytes = profileImage.read()
                 with open(targetImagePath, 'rb') as targetImage:
-                    image2_bytes = targetImage.read()  # Read file content
-
-                # Proceed with the image processing
+                    image2_bytes = targetImage.read()
                 image1_face_path = preprocess_and_save_image(image1_bytes, os.path.basename(profileImagePath))
                 image2_face_path = preprocess_and_save_image(image2_bytes, os.path.basename(targetImagePath))
-                
                 if not image1_face_path or not image2_face_path:
                     return Response({'result': False, 'confidence_percentage': 0.0, 'note': 'Face detection failed.'}, status=200)
-
-                result, confidence = compare_faces(image1_face_path, image2_face_path)
-                note = 'Comparison successful but faces do not match closely.' if not result else 'Comparison successful and faces match closely.'
+                result, confidence, note = compare_faces(image1_face_path, image2_face_path)
                 return Response({'result': result, 'confidence_percentage': confidence, 'note': note}, status=200)
-            
-            except Exception as e:
-                return Response({'error': f'Error processing images: {str(e)}'}, status=500)
-
+            finally:
+                # Ensure uploaded images are removed after processing
+                if os.path.exists(profileImagePath):
+                    os.remove(profileImagePath)
+                if os.path.exists(targetImagePath):
+                    os.remove(targetImagePath)
         return Response({'error': 'Unable to retrieve images from URLs'}, status=400)
-
-
-
-    @action(detail=False, methods=['POST'], url_path='compare-image', serializer_class=ImageSerializer)
+    
+    @action(
+        detail=False, 
+        methods=['POST'], 
+        url_path='compare-image', 
+        serializer_class=ImageSerializer
+    )
     def compare_image_file(self, request):
         if 'profileImage' not in request.FILES or 'targetImage' not in request.FILES:
             return Response({'error': 'Please upload two images'}, status=400)
@@ -120,8 +131,15 @@ class CompareImagesViewSet(viewsets.GenericViewSet):
         image2_bytes = targetImage.read()
         image1_face_path = preprocess_and_save_image(image1_bytes, profileImage.name)
         image2_face_path = preprocess_and_save_image(image2_bytes, targetImage.name)
-        if not image1_face_path or not image2_face_path:
-            return Response({'result': False, 'confidence_percentage': 0.0, 'note': 'Face detection failed.'}, status=200)
-        result, confidence = compare_faces(image1_face_path, image2_face_path)
-        note = 'Comparison successful but faces do not match closely.' if not result else 'Comparison successful and faces match closely.'
-        return Response({'result': result, 'confidence_percentage': confidence, 'note': note}, status=200)
+        try:
+            if not image1_face_path or not image2_face_path:
+                return Response({'result': False, 'confidence_percentage': 0.0, 'note': 'Face detection failed.'}, status=200)
+            
+            result, confidence, note = compare_faces(image1_face_path, image2_face_path)
+            return Response({'result': result, 'confidence_percentage': confidence, 'note': note}, status=200)
+        finally:
+            # Ensure uploaded images are removed after processing
+            if os.path.exists(image1_face_path):
+                os.remove(image1_face_path)
+            if os.path.exists(image2_face_path):
+                os.remove(image2_face_path)
